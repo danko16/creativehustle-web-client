9503df9e2a58e45b5af302357e91c5edffbaa50d
danang <danangekoyudanto1995@gmail.com>
2020-05-26 20:50:59 +0700

    register validation

--- src/register/index.js
+++ src/register/index.js
@@ -1,7 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ClassNames from 'classnames';
 import PropTypes from 'prop-types';
 
+import './register.css';
+
 function Register({ setDisplayModal }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const [error, setError] = useState({
+    name: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const nameValid = nameValidation();
+    const emailValid = emailValidation();
+    const passwordValid = passwordValidation();
+    const repeatValid = repeatPasswordValidation();
+
+    if (nameValid && emailValid && passwordValid && repeatValid) {
+      console.log('send registration');
+    }
+  }
+
+  function nameValidation() {
+    const nameFormat = /^[A-Za-z\s]{3,}$/i;
+    if (name.length < 4) {
+      setError((prevState) => ({
+        ...prevState,
+        name: 'Panjang nama minimal 4 karakter',
+      }));
+      return false;
+    }
+    if (!name.match(nameFormat)) {
+      setError((prevState) => ({
+        ...prevState,
+        name: 'Nama hanya boleh berisi huruf dan spasi',
+      }));
+      return false;
+    }
+
+    return true;
+  }
+
+  function emailValidation() {
+    //eslint-disable-next-line
+    const emailFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!email.match(emailFormat)) {
+      setError((prevState) => ({
+        ...prevState,
+        email: 'Email tidak valid',
+      }));
+      return false;
+    }
+
+    return true;
+  }
+
+  function passwordValidation() {
+    const passwordFormat = /^(?=.*[0-9])(?=.{8,})/;
+    if (password.length < 8) {
+      setError((prevState) => ({
+        ...prevState,
+        password: 'Panjang password minimal 8 karakter',
+      }));
+      return false;
+    }
+
+    if (!password.match(passwordFormat)) {
+      setError((prevState) => ({
+        ...prevState,
+        password: 'Password harus berisi minimal 1 angka',
+      }));
+      return false;
+    }
+
+    return true;
+  }
+
+  function repeatPasswordValidation() {
+    if (password !== repeatPassword || !password) {
+      setError((prevState) => ({
+        ...prevState,
+        repeatPassword: 'Confirm Password tidak cocok',
+      }));
+      return false;
+    }
+
+    return true;
+  }
+
   return (
     <div className="modal-content row align-items-center justify-content-center">
       <div className="col w-320px ">
@@ -21,8 +114,8 @@ function Register({ setDisplayModal }) {
         <h1 className="h3 font-weight-bold merriweather mt-4 pt-1">Akun Baru</h1>
         <p className="text-secondary h8 mt-1 mb-1 ">Learn. Build. Share to Make Money.</p>
 
-        <form method="POST" id="login" action="https://class.buildwithangga.com/register">
-          <input type="hidden" name="_token" value="SR9dLKREhwyJwnObwAS1Oqr1gL9VYcugcevSHVf5" />{' '}
+        <form onSubmit={handleSubmit}>
+          <input type="hidden" name="_token" value="SR9dLKREhwyJwnObwAS1Oqr1gL9VYcugcevSHVf5" />
           <div className="form-group form-group__icon mt-4">
             <label className="sr-only" htmlFor="username">
               Nama Lengkap
@@ -34,14 +127,29 @@ function Register({ setDisplayModal }) {
               required=""
               autoComplete="name"
               autoFocus=""
-              className="form-control"
+              className={ClassNames('form-control', { 'error-form': error.name })}
               aria-describedby="usernameHelp"
               placeholder="Nama Lengkap"
+              value={name}
+              onClick={() => {
+                setError((prevState) => ({
+                  ...prevState,
+                  name: '',
+                }));
+              }}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
             />
-            <small id="usernameHelp" className="form-text text-muted d-none">
-              Error Helper Text
-            </small>
           </div>
+
+          <small
+            className={ClassNames('error-text form-text d-none', {
+              'd-block': error.name,
+            })}
+          >
+            {error.name}
+          </small>
           <div className="form-group form-group__icon">
             <label className="sr-only" htmlFor="email">
               Email Address
@@ -53,13 +161,28 @@ function Register({ setDisplayModal }) {
               name="email"
               required=""
               autoComplete="email"
-              className="form-control "
+              className={ClassNames('form-control', { 'error-form': error.email })}
               aria-describedby="emailHelp"
               placeholder="Email Address"
+              value={email}
+              onClick={() => {
+                setError((prevState) => ({
+                  ...prevState,
+                  email: '',
+                }));
+              }}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             />
-
-            <small id="emailHelp_2" className="form-text text-muted d-none"></small>
           </div>
+          <small
+            className={ClassNames('error-text form-text d-none', {
+              'd-block': error.email,
+            })}
+          >
+            {error.email}
+          </small>
           <div className="form-group form-group__icon">
             <label className="sr-only" htmlFor="password">
               Password
@@ -70,15 +193,29 @@ function Register({ setDisplayModal }) {
               name="password"
               required=""
               autoComplete="current-password"
-              className="form-control"
+              className={ClassNames('form-control', { 'error-form': error.password })}
               id="password"
               aria-describedby="passwordHelp"
               placeholder="Password (Min. 8 Karakter)"
+              value={password}
+              onClick={() => {
+                setError((prevState) => ({
+                  ...prevState,
+                  password: '',
+                }));
+              }}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
-            <small id="passwordHelp" className="form-text text-muted d-none">
-              Error Helper Text
-            </small>
           </div>
+          <small
+            className={ClassNames('error-text form-text d-none', {
+              'd-block': error.password,
+            })}
+          >
+            {error.password}
+          </small>
           <div className="form-group form-group__icon">
             <label className="sr-only" htmlFor="password">
               Password
@@ -89,48 +226,29 @@ function Register({ setDisplayModal }) {
               name="password_confirmation"
               required=""
               autoComplete="current-password"
-              className="form-control"
+              className={ClassNames('form-control', { 'error-form': error.repeatPassword })}
               id="password2"
               aria-describedby="passwordHelp"
               placeholder="Confirm Password"
+              value={repeatPassword}
+              onClick={() => {
+                setError((prevState) => ({
+                  ...prevState,
+                  repeatPassword: '',
+                }));
+              }}
+              onChange={(e) => {
+                setRepeatPassword(e.target.value);
+              }}
             />
-            <small id="passwordHelp22" className="notice-password2 form-text text-muted d-none">
-              Confirm Password tidak cocok
-            </small>
           </div>
-          <div className="form-group form-group__icon">
-            <label className="sr-only" htmlFor="level_user">
-              Level User
-            </label>
-            <i className="material-icons mr-2">favorite</i>
-            <input
-              type="text"
-              name="level_user"
-              required=""
-              className="form-control"
-              id="level_user"
-              aria-describedby="passwordHelp"
-              placeholder="Keahlian, cth: Web Designer"
-            />
-            <small id="passwordHelp" className="form-text text-muted d-none">
-              Error Helper Text
-            </small>
-          </div>
-          <div className="form-group form-group__icon form-occupation">
-            <label className="sr-only">Occupation</label>
-            <select
-              className="form-control"
-              name="occupation"
-              id="occupation"
-              placeholder="Occupation"
-              required="required"
-            >
-              <option value="">-- Pilih Occupation--</option>
-              <option>Sekolah</option>
-              <option>Kuliah</option>
-              <option>Kerja</option>
-            </select>
-          </div>
+          <small
+            className={ClassNames('error-text form-text d-none', {
+              'd-block': error.repeatPassword,
+            })}
+          >
+            {error.repeatPassword}
+          </small>
           <div
             className="form-group form-group__icon"
             id="group_organization"
@@ -147,7 +265,7 @@ function Register({ setDisplayModal }) {
               aria-describedby="passwordHelp"
               placeholder=""
             />
-            <small id="organization" className="form-text text-muted d-none">
+            <small id="organization" className="form-text text-muted">
               Error Helper Text
             </small>
           </div>
