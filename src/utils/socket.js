3f05bacd3f9ad3d35e479616f11534db7a8398a5
danang <danangekoyudanto1995@gmail.com>
2020-05-29 23:26:47 +0700

    reserve some socket.io code

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,12 +1,25 @@
-const io = require('socket.io-client');
+import io from 'socket.io-client';
+import { store } from '../redux';
 
 const socket = io.connect('http://localhost:3000');
 
-export function emitterHandler({ name, data }) {
-  socket.emit(name, data);
-}
+socket.on('connect', function () {
+  const { auth } = store.getState();
+  socket.emit('authentication', auth.token);
+  socket.on('authenticated', function () {});
+  socket.on('unauthorized', function (err) {
+    console.log(err.message);
+  });
+});
 
 socket.on('error', function (err) {
   console.log('received socket error:');
   console.log(err);
 });
+
+export const loginEmmiter = (data) => {
+  console.log(data);
+  socket.emit('authentication', data.token);
+};
+
+export default socket;
