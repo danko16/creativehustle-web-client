67938d082f972ad8549bd24e0cf8430ef9c4f24f
danang <danangekoyudanto1995@gmail.com>
2020-06-23 16:44:27 +0700

    make it better

--- src/linding-page/index.js
+++ src/linding-page/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import Header from '../shared/header';
 import Footer from '../shared/footer';
 import Introduction from './introduction';
 import PopularClass from './popular-class';
-import Testimony from './testimony';
 import LearningPath from './learning-path';
-import RegisterOffer from './register-offer';
 
 import './landing-page.css';
 
@@ -16,9 +14,49 @@ function LandingPage() {
       <Header />
       <Introduction />
       <PopularClass />
-      <Testimony />
-      <RegisterOffer />
       <LearningPath />
+      <div
+        style={{
+          backgroundImage: 'url(/assets/img/cta-bg.png)',
+          padding: '54px 0',
+        }}
+      >
+        <div className="container">
+          <div
+            className="text-center py-4"
+            style={{
+              width: '70%',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              color: '#fff',
+            }}
+          >
+            <h2
+              style={{
+                fontWeight: 600,
+                fontSize: 34,
+                color: '#fff',
+              }}
+            >
+              Ayo segera daftar dan mulai belajar cara menghasilkan uang dengan cara kreatif
+            </h2>
+            <p
+              style={{
+                marginBottom: 28,
+              }}
+            >
+              Tidak perlu download / kartu kredit. Cukup daftar langsung belajar.
+            </p>
+            <button className="et_pb_button">
+              <Link to={`/kursus`} className="stretched-link">
+                <span className="sr-only">title for screen</span>
+              </Link>
+              <span>Lihat Kursus</span>
+              <i className="fa fa-angle-right" aria-hidden="true"></i>
+            </button>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
