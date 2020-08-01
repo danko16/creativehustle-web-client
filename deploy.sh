62509d228ad4862bafeadb968ed5004276838538
danang <danangekoyudanto1995@gmail.com>
2020-08-01 08:56:19 +0700

    sh script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-export NODE_ENV=production
-/home/danang/.nvm/versions/node/v12.18.0/bin/npm run build
-rm -rf /var/www/creativehustle.id/html/*
-cp -R build/* /var/www/creativehustle.id/html/
-nginx -t
+export NODE_ENV=production &&
+/home/danang/.nvm/versions/node/v12.18.0/bin/npm run build &&
+rm -rf /var/www/creativehustle.id/html/* &&
+cp -R build/* /var/www/creativehustle.id/html/ &&
+nginx -t &&
 systemctl restart nginx
