a656a56c4e5c59f7b15abce570ce122bafc8c1a8
danang <danangekoyudanto1995@gmail.com>
2020-08-01 08:50:15 +0700

    sh script build

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
-git pull origin master
 export NODE_ENV=production
-/home/danang/.nvm/versions/node/v12.18.0/bin/yarn install
-/home/danang/.nvm/versions/node/v12.18.0/bin/npm run build
+react-scripts build
 rm -rf /var/www/creativehustle.id/html/*
 cp -R build/* /var/www/creativehustle.id/html/
 nginx -t
